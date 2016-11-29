2854cc7086f705d39f279d2f32a574b6bdea28af
Daniel <daniel.eldar@gmail.com>
2016-11-29 19:27:02 +0000

    Added the motion controller simulation using the keyboard. Created a separate class which subclasses UMotionControllerComponent and overrides its TickComponent function. The class also allows for the use of the normal controller using the boolean ShouldBeSimulated from the blueprint or within code.

---
+++ Source/ProjectOrion/ProjectOrionMotionController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "MotionControllerComponent.h"
+#include "ProjectOrionMotionController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTORION_API UProjectOrionMotionController : public UMotionControllerComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
